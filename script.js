16b550fd6e70013bc0e26f13b37c0ff401d1607a
shikipon11 <shikipon11@gmail.com>
2026-06-14 13:47:19 +0900

    Update script.js

--- script.js
+++ script.js
@@ -3,289 +3,72 @@
 // =========================
 
 const projects = [
-
   {
-    number: 1,
-    title: "フォトスポット",
-    category: ["体験", "展示"],
-    place: "教室棟",
-    floor: "4階"
-  },
+  name: "ワッフル",
+  section: "模擬店",
+  price: "300円",
+  image: [""]
+},
   {
-    number: 2,
-    title: "お化け屋敷",
-    category: ["体験"],
-    place: "教室棟",
-    floor: "4階"
-  },
-  {
-    number: 3,
-    title: "宝探し",
-    category: ["体験"],
-    place: "教室棟",
-    floor: "4階"
-  },
-  {
-    number: 4,
-    title: "写真",
-    category: ["展示"],
-    place: "教室棟",
-    floor: "4階",
-    image: ["images/写真班.png"]
-  },
-  {
-    number: 5,
-    title: "CAM",
-    category: ["展示"],
-    place: "教室棟",
-    floor: "4階",
-    image: ["images/CAM班.PNG"]
-  },
-  {
-    number: 6,
-    title: "総合社会研究",
-    category: ["体験", "展示"],
-    place: "教室棟",
-    floor: "3階",
-    image: ["images/総合社会研究班.JPG"]
-  },
-  {
-    number: 7,
-    title: "書道",
-    category: ["体験", "展示"],
-    place: "教室棟",
-    floor: "3階",
-    image: ["images/書道班.jpg"]
-  },
-  {
-    number: 8,
-    title: "手相",
-    category: ["体験"],
-    place: "教室棟",
-    floor: "3階",
-    image: ["images/手相同好会.jpg"]
-  },
-  {
-    number: 9,
-    title: "競技かるた",
-    category: ["体験"],
-    place: "教室棟",
-    floor: "2階"
-  },
-  {
-    number: 10,
-    title: "美術",
-    category: ["体験", "展示", "販売"],
-    places: [{
-        building: "教室棟",
-        floor: "2階"
-      },
+  name: "チョコバナナ",
+  section: "模擬店",
+  price: "300円",
+  image: [""]
+},
+    {
+  name: "タピオカ",
+  section: "模擬店",
+  price: "300円",
+  image: [""]
+},
+    {
+  name: "ポップコーン",
+  section: "模擬店",
+  price: "300円",
+  image: [""]
+},
+    {
+  name: "フランクフルト",
+  section: "模擬店",
+  price: "300円",
+  image: [""]
+},
+    {
+  name: "焼き鳥",
+  section: "模擬店",
+  price: "300円",
+  image: [""]
+},
+    {
+  name: "レモネード",
+  section: "模擬店",
+  price: "300円",
+  image: [""]
+},
+    {
+  name: "クレープ",
+  section: "模擬店",
+  price: "300円",
+  image: [""]
+},
+    {
+  name: "たこ焼き",
+  section: "模擬店",
+  price: "300円",
+  image: [""]
+},
       {
-        building: "管理棟",
-        floor: "1階"
-      },
+  name: "あれこれ",
+  section: "飲食",
+  price: "300円",
+  image: [""]
+},
       {
-        building: "その他",
-        floor: "渡り廊下"
-      }
-    ],
-    image: ["images/美術班.png"]
-  },
-  {
-    number: 11,
-    title: "文芸",
-    category: ["体験", "展示"],
-    place: "教室棟",
-    floor: "2階",
-    image: ["images/文芸班.png"]
-  },
-  {
-    number: 12,
-    title: "松尾祭地域応援",
-    category: ["展示", "飲食"],
-    place: "教室棟",
-    floor: "1階",
-    image: ["images/松尾菜.png"]
-  },
-  {
-    number: 13,
-    title: "全校制作",
-    category: ["展示"],
-    place: "その他",
-    floor: "渡り廊下"
-  },
-  {
-    number: 14,
-    title: "カンボジア井戸プロ",
-    category: ["販売"],
-    place: "管理棟",
-    floor: "2階",
-    image: ["images/カンボジア.png"]
-  },
-  {
-    number: 15,
-    title: "数学",
-    category: ["体験"],
-    place: "管理棟",
-    floor: "2階",
-    image: ["images/数学研究同好会.png"]
-  },
-  {
-    number: 16,
-    title: "被服",
-    category: ["体験", "販売"],
-    place: "管理棟",
-    floor: "2階",
-    image: ["images/被服班.png"]
-  },
-  {
-    number: 17,
-    title: "英語",
-    category: ["体験", "飲食"],
-    place: "管理棟",
-    floor: "2階",
-    image: ["images/英語班.jpeg"]
-  },
-  {
-    number: 18,
-    title: "図書",
-    category: ["体験", "飲食"],
-    place: "管理棟",
-    floor: "2階",
-    image: ["images/図書館.png"]
-  },
-  {
-    number: 19,
-    title: "茶道",
-    category: ["飲食", "パフォーマンス"],
-    place: "管理棟",
-    floor: "1階",
-    image: ["images/茶道班.png"]
-  },
-  {
-    number: 20,
-    title: "ものづくり販売",
-    category: ["販売"],
-    place: "その他",
-    floor: "昇降口（中）"
-  },
-  {
-    number: 21,
-    title: "食品科学",
-    category: ["飲食"],
-    place: "管理棟",
-    floor: "1階",
-    image: ["images/食品科学班.JPG"]
-  },
-  {
-    number: 22,
-    title: "化学",
-    category: ["体験", "パフォーマンス"],
-    place: "管理棟",
-    floor: "3階",
-    image: ["images/化学班.png"]
-  },
-  {
-    number: 23,
-    title: "天文気象",
-    category: ["展示"],
-    place: "管理棟",
-    floor: "3階",
-    image: ["images/天文班.png"]
-  },
-  {
-    number: 24,
-    title: "棋道",
-    category: ["体験"],
-    place: "管理棟",
-    floor: "3階"
-  },
-  {
-    number: 25,
-    title: "クイズ",
-    category: ["体験"],
-    place: "管理棟",
-    floor: "3階",
-    image: ["images/総合文化部クイズ班_page-0001.jpg"]
-  },
-  {
-    number: 26,
-    title: "吹奏楽/書道/ダンス",
-    category: ["パフォーマンス"],
-    place: "その他",
-    floor: "第一体育館メイン",
-    images: ["images/吹奏楽班コンサート.jpg", "images/書道班.jpg", "images/ダンス班_page-0001.jpg"]
-  },
-  {
-    number: 27,
-    title: "卓球",
-    category: ["体験"],
-    place: "その他",
-    floor: "第一体育館ギャラリー"
-  },
-  {
-    number: 28,
-    title: "ちびっこ広場",
-    category: ["体験"],
-    place: "その他",
-    floor: "定時棟 1階"
-  },
-  {
-    number: 29,
-    title: "合唱/小さな音楽会",
-    category: ["パフォーマンス"],
-    place: "その他",
-    floor: "音楽室",
-    images: ["images/混声合唱班_page-0001.jpg", "images/小さな音楽会.png"]
-  },
-  {
-    number: 30,
-    title: "剣道",
-    category: ["体験"],
-    place: "その他",
-    floor: "格技室"
-  },
-  {
-    number: 31,
-    title: "弓道",
-    category: ["体験"],
-    place: "その他",
-    floor: "ピロティ",
-    image: ["images/弓道班.JPG"]
-  },
-  {
-    number: 32,
-    title: "現代音楽/吹奏楽",
-    category: ["パフォーマンス"],
-    place: "その他",
-    floor: "中庭（野外ステージ）",
-    images: ["images/現代音楽_page-0001.jpg", "images/吹奏楽班ステージ.jpg"]
-  },
-  {
-    number: 33,
-    title: "放送",
-    category: ["体験"],
-    place: "その他",
-    floor: "昇降口(外)",
-    image: ["images/放送班 - 1.png"]
-  },
-  {
-    number: 34,
-    title: "演劇",
-    category: ["パフォーマンス"],
-    place: "その他",
-    floor: "同窓会館　1階",
-    image: ["images/演劇班.jpg"]
-  },
-  {
-    number: 35,
-    title: "室内楽/合唱",
-    category: ["パフォーマンス"],
-    place: "その他",
-    floor: "同窓会館　2階",
-    images: ["images/室内楽班.png", "images/混声合唱班_page-0001.jpg"]
-  }
-
+  name: "いろいろ",
+  section: "地域応援",
+  price: "300円",
+  image: [""]
+}
 ];
 
 // =========================
@@ -509,9 +292,11 @@ tabButtons.forEach(button => {
 // =========================
 function renderNumberView() {
 
-  contentArea.innerHTML = "";
-
-  createProjectList(projects);
+  createProjectList(
+    products.filter(
+      p => p.section === "模擬店"
+    )
+  );
 
 }
 // =========================
@@ -519,9 +304,11 @@ function renderNumberView() {
 // =========================
 function renderCategoryView() {
 
-  contentArea.innerHTML = "";
-
-  createProjectList(projects);
+  createProjectList(
+    products.filter(
+      p => p.section === "飲食係"
+    )
+  );
 
 }
 // =========================
@@ -529,9 +316,11 @@ function renderCategoryView() {
 // =========================
 function renderPlaceView() {
 
-  contentArea.innerHTML = "";
-
-  createProjectList(projects);
+  createProjectList(
+    products.filter(
+      p => p.section === "地域応援"
+    )
+  );
 
 }
 //仮
@@ -548,12 +337,8 @@ function createProjectList(data) {
     card.innerHTML = `
       <div class="project-top">
 
-        <div class="project-circle">
-          ${project.number}
-        </div>
-
         <div class="project-title">
-          ${project.title}
+          ${project.name}
         </div>
 
       </div>
